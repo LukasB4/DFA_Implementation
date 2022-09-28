239107c07dae8b0181b8a516ce152679abb6b7aa
LukasB4 <103784087+LukasB4@users.noreply.github.com>
2022-09-27 18:54:12 -0600

    Added Empty state, edited between

--- src/GumballMachine.java
+++ src/GumballMachine.java
@@ -6,14 +6,19 @@ public class GumballMachine {
     //private instance variables for each possible state
     private State hasQuarterState;
     private State noQuarterState;
-    
+    private State emptyState;    
     //current state the machine is in
     private State currentState;
+    //number of gumballs in machine
+    private int gumNum;
     
     public GumballMachine() {
         //initialize possible state instance variables
         hasQuarterState = new HasQuarterState(this);
         noQuarterState = new NoQuarterState(this);
+        emptyState = new EmptyState(this);
+        gumNum = 3;
+
         
         //set initial current state
         currentState = noQuarterState;
@@ -23,6 +28,11 @@ public class GumballMachine {
         this.currentState = state;
     }
     
+    public State getEmptyState()
+    {
+        return emptyState;
+    }
+
     public State getHasQuarterState() {
         return hasQuarterState;
     }
@@ -40,6 +50,19 @@ public class GumballMachine {
     }
     
     public void turnCrank() {
-        currentState.turnCrank();
+        if(!emptyState.getEmptyState())
+        {
+            currentState.turnCrank();
+        }
+        else
+        {
+            System.out.println("There are no gumballs left. Refilling...");
+            refill();
+        }
+        emptyState.setEmpty(gumNum);
+    }
+    public void refill()
+    {
+        gumNum = 3;
     }
 }
